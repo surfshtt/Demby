bd68e96875b93baa8d4bb2d6918bb95891b56c35
surfshtt <fk8ghtt@gmail.com>
2024-11-25 15:23:49 +0300

    1.2 in the project was added a reg page and connection between pages

--- app/src/main/java/com/example/dembyapp/MenuActivity.java
+++ app/src/main/java/com/example/dembyapp/MenuActivity.java
@@ -1,9 +1,6 @@
 package com.example.dembyapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    TextView go_reg_button;
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        go_reg_button = findViewById(R.id.go_reg_button);
-        go_reg_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToRegActivity(v);
-            }
-        });
-    }
-
-    public void goToRegActivity(View v){
-        Intent intent = new Intent(this, RegistrationActivity.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/dembyapp/RegistrationActivity.java
+++ app/src/main/java/com/example/dembyapp/RegistrationActivity.java
@@ -11,32 +11,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    TextView go_reg_button;
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        go_reg_button = findViewById(R.id.go_reg_button);
-        go_reg_button.setOnClickListener(new View.OnClickListener() {
+        TextView go_log_button = findViewById(R.id.go_log_button);
+        go_log_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToRegActivity(v);
+                goToLogActivity(v);
             }
         });
     }
 
-    public void goToRegActivity(View v){
-        Intent intent = new Intent(this, RegistrationActivity.class);
+    public void goToLogActivity(View v){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
