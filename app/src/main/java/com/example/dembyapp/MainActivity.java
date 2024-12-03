f6050920af9b38fa3e9063d3cb9e2c60b38766fb
surfshtt <fk8ghtt@gmail.com>
2024-12-03 17:16:03 +0300

    1.6 Add logic for Login Button, data validation, and starting a menu activity

--- app/src/main/java/com/example/dembyapp/MainActivity.java
+++ app/src/main/java/com/example/dembyapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.dembyapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import Data.DatabaseHandler;
+import Model.User;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView go_reg_button;
+    EditText user_name_field, password_field;
+
+    DatabaseHandler databaseHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
                 goToRegActivity(v);
             }
         });
+
+        user_name_field = findViewById(R.id.user_name_field);
+        password_field = findViewById(R.id.password_field);
+
+        databaseHandler = new DatabaseHandler(this);
     }
 
     public void goToRegActivity(View v){
@@ -40,4 +55,40 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void LogButton_Click(View v) {
+        String userName = user_name_field.getText().toString();
+        String password = password_field.getText().toString();
+
+        if(fieldValidation(userName,password)){
+            showWarn("Вход Успешен!");
+            Log.i("Login", "Correct data. Username: " + userName + " Password: " + password);
+
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        }
+        else{
+            showWarn("Некорректные данные!");
+            Log.i("Login", "Invalid data. Username: " + userName + " Password: " + password);
+        }
+    }
+
+    private boolean fieldValidation(String userName, String password){
+
+        if(userName.isEmpty() || userName.contains(" ") || userName.contains("SELECT") || userName.contains("WHERE"))
+            return false;
+
+        User userData = databaseHandler.getUser(userName);
+
+        if(password.isEmpty() || password.contains(" ") || password.length() < 8)
+            return false;
+
+        if(userData == null)
+            return false;
+
+        return userData.getPassword().equals(password);
+    }
+
+    public void showWarn(String text){
+        Toast.makeText(this,text,Toast.LENGTH_SHORT).show();
+    }
 }
