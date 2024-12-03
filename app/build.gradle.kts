7ab380c808c33d390464f011c3370d885d278c69
surfshtt <fk8ghtt@gmail.com>
2024-12-03 20:25:44 +0300

    2.0 started work on main menu, added bottom menu and items in it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.annotations)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
