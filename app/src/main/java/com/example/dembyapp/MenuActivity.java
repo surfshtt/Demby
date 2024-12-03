fde566908287d86f26d7d8d87960a19dbc1ac684
surfshtt <fk8ghtt@gmail.com>
2024-12-03 21:08:56 +0300

    2.0 started work on main menu, added bottom menu and items in it. Three fragments were created for future work on them

--- app/src/main/java/com/example/dembyapp/MenuActivity.java
+++ app/src/main/java/com/example/dembyapp/MenuActivity.java
@@ -7,6 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -20,5 +25,9 @@ public class MenuActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView menu_field = findViewById(R.id.menu_field);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(menu_field, navController);
     }
 }
